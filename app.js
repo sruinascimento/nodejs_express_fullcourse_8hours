90593bf23c31f4b137255cd057281322ec82d274
Rui Nascimento <sruy19@gmail.com>
2021-11-24 20:39:01 -0300

    app modified

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 //__dirname - path to current directory
+// console.log(`${__dirname}`);
 
-console.log(`${__dirname}`);
+// console.log(`${process.cwd()}`);
+// let time = 0;
+// setInterval(() => {
+//     console.log(`>>>> ${time++} -> Hello word`);
+// }, 1000);
+
+
+// const values = require("./modulos");
+// const {secret, devJR, sayHi} = require("./modulos");
+
+// // console.log(values.devJR);
+// // console.log(values.secret);
+// // console.log(values.sayHi("Rui"));
+// // console.log(values);
+
+// console.log(devJR);
+// console.log(secret);
+// console.log(sayHi("Rui"));
