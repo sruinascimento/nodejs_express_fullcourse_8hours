d69fc26a67a34b1eb8316fb6929cffd6cd8f245c
Rui Nascimento <sruy19@gmail.com>
2021-11-24 20:38:00 -0300

    arquivo para aprender a criar e exportar módulos

---
+++ modulos.js
@@ -0,0 +1,13 @@
+/**
+ * Modulos em javascrito
+ * module.exports e export os objetos
+ */
+
+
+module.exports = {
+    secret: "1234", 
+    devJR: "Ruizinho",
+    sayHi: (name) => {
+        return ">>>> Hi "+name;
+    }    
+};
