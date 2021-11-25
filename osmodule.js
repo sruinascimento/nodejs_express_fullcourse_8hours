4d4f220364112090e68cabb90f014c684eb81360
Rui Nascimento <sruy19@gmail.com>
2021-11-25 08:56:02 -0300

    módulo OS para obter informações do systema operacional

---
+++ osmodule.js
@@ -0,0 +1,18 @@
+const os = require("os");
+
+//info about current user
+const user = os.userInfo();
+
+//method returns the system uptime in seconds
+console.log(`The system uptime is ${os.uptime()} minutes `)
+
+const osModules = {
+    name: os.type(),
+    release: os.release(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    cpus: os.cpus()[0]
+}
+
+
+console.log(osModules)
